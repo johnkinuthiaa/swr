90a3b767c4f0f1604ad685e06d21aff74b92ef03
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-04 22:17:48 +0300

    use swr

--- README.md
+++ README.md
@@ -25,3 +25,28 @@ const fetchData=(async()=>{
 ```shell
 npm install swr
 ```
+### Using the useSWR hook
+
+```javascript
+//import useSWR
+import useSWR from "swr";
+// Normal fetcher function
+const fetchData =(async ()=>{
+    const response =await fetch(API_URL+GET_ALL,{
+        method:"GET"
+    })
+    if(response.ok){
+        const data =await response.json()
+        setTimeout(()=>{
+            setPets(data.sort((a,b)=>b.id-a.id))
+        },2000)
+
+    }else{
+        throw new Error("Error fetching data from api")
+    }
+
+})
+const{isLoading,error} =useSWR(API_URL+GET_ALL,fetchData)
+if(error)return <div>Failed to load</div>
+if(isLoading) return <div>Loading...</div>
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
 import './App.css'
 import {useEffect, useState} from "react";
-
+import useSWR from "swr";
+import Spinner from "./components/Spinner.jsx";
 function App() {
   const [pet,setPets] =useState([])
   const API_URL ="http://localhost:8080/api/v1/pets"
   const ADD_API_ENDPOINT ="/add/new"
   const GET_ALL ="/all"
-  useEffect(()=>{
-    fetchData()
-  },[])
+  // using useEffect
+  // useEffect(()=>{
+  //   fetchData()
+  // },[])
+
+  // use swr hook
+
 
   const fetchData =(async ()=>{
     const response =await fetch(API_URL+GET_ALL,{
@@ -17,18 +22,30 @@ function App() {
     })
     if(response.ok){
       const data =await response.json()
-      setPets(data)
+      setTimeout(()=>{
+        setPets(data.sort((a,b)=>b.id-a.id))
+      },10000)
+
     }else{
       throw new Error("Error fetching data from api")
     }
 
   })
+  const{isLoading,error} =useSWR(API_URL+GET_ALL,fetchData)
+  if(error)return <div>Failed to load</div>
+  if(isLoading) return<Spinner/>
+  const petContainer ={
+    display:"flex",
+    flexWrap:"wrap",
+    gap:"3px"
+
+  }
 
   return (
-    <div>
+    <div style={petContainer}>
       {pet.length>0?(
           pet.map((item)=>(
-              <div key={item.id}>
+              <div key={item.id} style={{padding:"10px",width:"300px"}}>
                 <img src={item.image} alt={item.name}/>
                 <p>{item.name}</p>
                 <p>{item.price}</p>
@@ -37,7 +54,7 @@ function App() {
               </div>
           ))
       ) : (
-          <div>pet list is empty</div>
+          <Spinner/>
       )}
     </div>
   )
